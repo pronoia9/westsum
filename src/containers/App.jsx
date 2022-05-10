975b1e4c713ba9eec18d21091c2e0a0b8a20818f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-10 11:35:40 +0300

    add state

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: [], // will contain the fetched quotes
+      number: 0,  // will be the number of quotes the user will choose to display
+    };
+  }
   render() {
     return (
       <div className='App'>
