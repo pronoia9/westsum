2ebe1aa74af35ef5432efc718461c03dad273a85
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-10 11:41:51 +0300

    add normalize css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// components
 import App from './containers/App';
+// styles
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
