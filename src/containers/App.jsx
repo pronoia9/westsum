a824dd510e44f31fbe11a53b72a8b4393f5e0de1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-10 11:42:46 +0300

    remove cra components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,23 +6,11 @@ class App extends Component {
     super();
     this.state = {
       quotes: [], // will contain the fetched quotes
-      number: 0,  // will be the number of quotes the user will choose to display
+      number: 0, // will be the number of quotes the user will choose to display
     };
   }
   render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className='tx-smooth'></div>;
   }
 }
 
