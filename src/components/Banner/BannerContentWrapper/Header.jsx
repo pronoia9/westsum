740f41079c30f2b840c12b1d371c5ea528459668
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:27:09 +0300

    rename css classes

--- src/components/Banner/BannerContentWrapper/Header.jsx
+++ src/components/Banner/BannerContentWrapper/Header.jsx
@@ -4,7 +4,7 @@ import Div from '../../General/Div';
 
 export default function Header({ title, subtitle }) {
   return (
-    <Div divs={['o-col-10 o-col-offset-1 o-col-9@lg o-col-offset-3@lg', 'c-maker']}>
+    <Div divs={['o-col-10 o-col-offset-1 o-col-9@lg o-col-offset-3@lg', 'c-hero__desc']}>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
     </Div>
