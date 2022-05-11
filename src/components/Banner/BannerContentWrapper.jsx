75d0ba58a7ff8341925d6540b816c6e707a9ea48
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:38:21 +0300

    adjust urls

--- src/components/Banner/BannerContentWrapper.jsx
+++ src/components/Banner/BannerContentWrapper.jsx
@@ -8,7 +8,7 @@ import Div from '../General/Div';
 export default function BannerContentWrapper({ urls }) {
   return (
     <Div divs={['o-container u-post-relative u-zi-10', 'o-row u-pos-relative']}>
-      <Maker url1={urls[0]} url2={urls[1]} />
+      <Maker urls={urls} />
       <Header title='Westsum' subtitle='A little quote generator of Kanye West.' />
       <Image />
     </Div>
--- src/components/Banner/BannerContentWrapper/Maker.jsx
+++ src/components/Banner/BannerContentWrapper/Maker.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Maker.css';
 
-const Maker = ({ url1, url2 }) => {
+const Maker = ({ urls }) => {
   return (
     <div className='c-maker'>
       <p className='c-maker__text'>
-        <a className='c-maker__link strike' href={url2} target='_blank' rel='noreferer'>He made this.</a>
-        <span><a className='c-maker__link' href={url1} target='_blank' rel='noreferer'> I</a> made this?</span>
+        <a className='c-maker__link strike' href={urls[1]} target='_blank' rel='noreferer'>He made this.</a>
+        <span><a className='c-maker__link' href={urls[0]} target='_blank' rel='noreferer'> I</a> made this?</span>
       </p>
     </div>
   );
