97842b85ca69085da814ed503b204fd8616e0531
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 05:28:28 +0300

    change text order

--- src/components/Banner/BannerContentWrapper/Maker.jsx
+++ src/components/Banner/BannerContentWrapper/Maker.jsx
@@ -5,8 +5,8 @@ const Maker = ({ url1, url2 }) => {
   return (
     <div className='maker'>
       <p className='maker__text'>
-        <span><a href={url1} target='_blank' rel='noreferer'>I</a> made this? </span>
-        <a className='maker__link' href={url2} target='_blank' rel='noreferer'>He made this.</a>
+        <a className='maker__link strike' href={url2} target='_blank' rel='noreferer'>He made this.</a>
+        <span><a className='maker__link' href={url1} target='_blank' rel='noreferer'> I</a> made this?</span>
       </p>
     </div>
   );
