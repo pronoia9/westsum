75812e1e0727262f843920f852de3f6fba696144
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 04:55:14 +0300

    fix classname

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = (props) => {
-  return <div className='banner-container banner--one u-pos-relative'>{props.children}</div>;
+  return <div className='banner-container banner-container--one u-pos-relative'>{props.children}</div>;
 };
 
 export default Banner;
