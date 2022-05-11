7301bb7f38c48dba249964e1d74b127daa5c8c2a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:28:48 +0300

    rename css classes

--- src/components/Banner/BannerContentWrapper/Image.jsx
+++ src/components/Banner/BannerContentWrapper/Image.jsx
@@ -5,7 +5,7 @@ import './Image.css';
 export default function Image() {
   animation();
   return (
-    <div className='image-container'>
+    <div className='c-hero__image'>
       <img src={require('../../../images/kanye_1.png')} className='js-kanye-img-1 u-width-12 u-zi-10' alt='Kanye YEAH' />
       <img src={require('../../../images/kanye_2.png')} className='js-kanye-img-2 u-width-12 u-zi-5' alt='Kanye Again' />
       <img src={require('../../../images/kanye_3.png')} className='js-kanye-img-3 u-width-12 u-zi-1' alt={`It's all Kanye`} />
