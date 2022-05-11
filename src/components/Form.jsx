91e4b5d7be028c866c05802e067a7bd09c78c07d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 06:30:21 +0300

    rename styles classes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,8 +5,8 @@ const Form = () => {
   return (
     <Container divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
       <form action='/' method='POST'>
-        <Container divs={['o-flexy@lg o-flexy--top form-container__bar']}>
-          <Container divs={['o-flexy__block form-container__block tx-center', 'o-inline']}>
+        <Container divs={['o-flexy@lg o-flexy--top form__bar']}>
+          <Container divs={['o-flexy__block form__block tx-center', 'o-inline']}>
             <Container divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like</Container>
             <Container divs={['o-inline__item u-mrg-r-1']}>
               <input
@@ -20,7 +20,7 @@ const Form = () => {
             </Container>
             <Container divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Container>
           </Container>
-          <input />
+          <input type='submit' className='o-flexy__item form__button js-input-bg generate-button' />
         </Container>
       </form>
     </Container>
