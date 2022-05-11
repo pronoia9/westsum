cddbd82824cf300b9faea4b6c618839c7e28a04c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:35:37 +0300

    add urls prop

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,10 +5,10 @@ import './Banner/BannerContentWrapper';
 import './Banner.css';
 import BannerContentWrapper from './Banner/BannerContentWrapper';
 
-const Banner = (props) => {
+const Banner = ({ urls }) => {
   return (
     <div className='c-hero c-hero--one u-pos-relative'>
-      <BannerContentWrapper />
+      <BannerContentWrapper urls={urls} />
       <div className='c-hero__kanye c-hero__kanye--two js-hero-kanye-two'></div>
       <div className='c-hero__kanye c-hero__kanye--three js-hero-kanye-three'></div>
     </div>
