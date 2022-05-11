2d69a2947304d3e63e8eb4dee7288e82a376bb71
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 04:59:59 +0300

    add banner content wrapper

---
+++ src/components/Banner/BannerContentWrapper.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// components
+// styles
+import './BannerContentWrapper.css';
+
+const BannerContentWrapper = () => {
+  return <div></div>;
+};
+
+export default BannerContentWrapper;
