ef5cad55f82536bb6a19257a044462d4efd6c9d8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 07:54:43 +0300

    move onsubmit to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,10 +3,10 @@ import './Form.css';
 import Div from './General/Div';
 import Input from './Form/Input';
 
-const Form = ({ onchange, onSubmit }) => {
+const Form = ({ onChange, onSubmit }) => {
   return (
     <Div divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
-      <form action='/' method='POST'>
+      <form onSubmit={(e) => onSubmit(e)}>
         <Div divs={['o-flexy@lg o-flexy--top form__bar']}>
           <Div divs={['o-flexy__block form__block tx-center', 'o-inline']}>
             <Div divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like </Div>
@@ -18,17 +18,12 @@ const Form = ({ onchange, onSubmit }) => {
                 type='number'
                 max='101'
                 min='1'
-                onChange={(e) => onchange(e)}
+                onChange={(e) => onChange(e)}
               />
             </Div>
             <Div divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quote(s) of that Kanye West.</Div>
           </Div>
-          <Input
-            type='submit'
-            value='Receive the West'
-            className='o-flexy__item form__button js-input-bg generate-button'
-            onSubmit={(e) => onSubmit(e)}
-          />
+          <Input type='submit' value='Receive the West' className='o-flexy__item form__button js-input-bg generate-button' />
         </Div>
       </form>
     </Div>
