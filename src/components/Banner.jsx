7c17608c609c47bb381872e468ae31d9f52f4a2d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 05:07:18 +0300

    return banner content wrapper

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 import './Banner/BannerContentWrapper';
 // styles
 import './Banner.css';
+import BannerContentWrapper from './Banner/BannerContentWrapper';
 
 const Banner = (props) => {
-  return <div className='banner-container banner-container--one u-pos-relative'>{props.children}</div>;
+  return (
+    <div className='banner-container banner-container--one u-pos-relative'>
+      <BannerContentWrapper />
+    </div>
+  );
 };
 
 export default Banner;
