a4839a02682af42a638b4c9bd3e530eddca63c83
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 05:26:24 +0300

    change props

--- src/components/Banner/BannerContentWrapper.jsx
+++ src/components/Banner/BannerContentWrapper.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // components
 import Maker from './BannerContentWrapper/Maker';
+import Header from './BannerContentWrapper/Header';
 // styles
 import './BannerContentWrapper.css';
 
@@ -8,8 +9,9 @@ const BannerContentWrapper = () => {
   return (
     <div className='banner-content-wrapper u-post-relative u-zi-10'>
       <div className='o-row u-pos-relative'>
-        <Maker url='https://www.seanhalpin.design/work/jeffsum/' />
+        <Maker url1='' url2='https://www.seanhalpin.design/work/jeffsum/' />
         {/* title/header */}
+        <Header title='Westsum' subtitle='A little quote generator of Kanye West.' />
         {/* image */}
       </div>
     </div>
--- src/components/Banner/BannerContentWrapper/Maker.jsx
+++ src/components/Banner/BannerContentWrapper/Maker.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Maker.css';
 
-const Maker = ({ url }) => {
+const Maker = ({ url1, url2 }) => {
   return (
     <div className='maker'>
       <p className='maker__text'>
-        <span>I made this?</span>
-        <a className='maker__link' href={url} target='_blank' rel='noreferer'>He made this.</a>
+        <span><a href={url1} target='_blank' rel='noreferer'>I</a> made this? </span>
+        <a className='maker__link' href={url2} target='_blank' rel='noreferer'>He made this.</a>
       </p>
     </div>
   );
