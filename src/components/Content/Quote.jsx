cd247ffed70135e83c91812af191f9896a19902d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 18:09:57 +0300

    add variation to kaynemojis

--- src/components/Content/Quote.jsx
+++ src/components/Content/Quote.jsx
@@ -6,14 +6,16 @@ export default function Quote({ quote, index }) {
     <div className='c-quotes'>
       {index === 0 ? <hr /> : null}
       <div className={`c-quote-group ${index % 2 ? 'right' : ''}`}>
-        <img src={require('../../images/kanyemoji_0.png')} />
+        <img src={require(`../../images/kanyemoji_${index.toString().charAt(index.toString().length - 1)}.png`)} />
         <div className='c-quote__container'>
           <blockquote>
             <p className='c-quote__quote'>{quote}</p>
           </blockquote>
           <cite>
-            <span>Kanye West</span><br />
-            American rapper.<br />
+            <span>Kanye West</span>
+            <br />
+            American rapper.
+            <br />
             Also record producer, entrepreneur, and fashion designer.
           </cite>
         </div>
