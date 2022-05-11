cc55698a2af7d8b9d718c2772f6f1c05c4778ac6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 05:43:01 +0300

    add image component

---
+++ src/components/Banner/BannerContentWrapper/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Image.css';
+
+const Image = () => {
+  return (
+    <div className='image-container'>
+      <img src='../../../images/1.jpg' className='u-width-12 js-jeff-img-1 u-zi-10' alt='Jeff YEAH' />
+    </div>
+  );
+};
+
+export default Image;
