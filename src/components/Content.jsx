d63373f605a2a222e7b1f960859163ea6c8ac6ac
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 18:03:10 +0300

    import quote component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Div from './General/Div';
+import Quote from './Content/Quote';
 import './Content.css';
 
 export default function Content({ quotes }) {
   return (
     <Div divs={['c-content', 'o-container', 'o-row', 'o-col-10 o-col-offset-1 o-col-8@md o-col-offset-2@md', 'generated-text']}>
-      {quotes.map(q => <p key={q}>{q}</p>)}
+      {quotes.map((q, i) => (
+        <Quote key={i} quote={q} index={i} />
+      ))}
     </Div>
   );
 }
