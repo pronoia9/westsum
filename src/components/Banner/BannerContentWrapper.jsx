023eb5f056946543ee4a70b2639cc8c1c8405b86
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 06:45:27 +0300

    change banner content wrapper styles

--- src/components/Banner/BannerContentWrapper.jsx
+++ src/components/Banner/BannerContentWrapper.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import Maker from './BannerContentWrapper/Maker';
 import Header from './BannerContentWrapper/Header';
 import Image from './BannerContentWrapper/Image';
-// styles
-import './BannerContentWrapper.css';
 
 const BannerContentWrapper = () => {
   return (
-    <div className='banner-content-wrapper u-post-relative u-zi-10'>
+    <div className='o-container u-post-relative u-zi-10'>
       <div className='o-row u-pos-relative'>
         <Maker url1='' url2='https://www.seanhalpin.design/work/jeffsum/' />
         <Header title='Westsum' subtitle='A little quote generator of Kanye West.' />
