0608d358a2d59f7503ce9e049a79e5d251511fc3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 05:23:13 +0300

    make header component

---
+++ src/components/Banner/BannerContentWrapper/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ({ title, subtitle }) => {
+  return (
+    <div className='header__desc'>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </div>
+  );
+};
+
+export default Header;
