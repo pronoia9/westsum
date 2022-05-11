b1c5c9973b8be73563a7550b6bf16dc03cbc56cb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 06:27:48 +0300

    redo container/divs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,19 +5,22 @@ const Form = () => {
   return (
     <Container divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
       <form action='/' method='POST'>
-        <Container
-          divs={['o-flexy@lg o-flexy--top form-container__bar', 'o-flexy__block form-container__block tx-center', 'o-inline']}>
-          <Container divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like</Container>
-          <Container divs={['o-inline__item u-mrg-r-1']}>
-            <input
-              class='c-input c-input-static--md form-input__mini'
-              name='numberOfQuotes'
-              value='5'
-              type='number'
-              max='101'
-              min='1' />
+        <Container divs={['o-flexy@lg o-flexy--top form-container__bar']}>
+          <Container divs={['o-flexy__block form-container__block tx-center', 'o-inline']}>
+            <Container divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like</Container>
+            <Container divs={['o-inline__item u-mrg-r-1']}>
+              <input
+                class='c-input c-input-static--md form-input__mini'
+                name='numberOfQuotes'
+                value='5'
+                type='number'
+                max='101'
+                min='1'
+              />
+            </Container>
+            <Container divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Container>
           </Container>
-          <Container divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Container>
+          <input />
         </Container>
       </form>
     </Container>
