3bee867d5c8ef57c9922ab52f2657b0317ebbb71
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:24:10 +0300

    rename css classes

--- src/components/Banner/BannerContentWrapper/Header.jsx
+++ src/components/Banner/BannerContentWrapper/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = ({ title, subtitle }) => {
   return (
     <div className='o-col-10 o-col-offset-1 o-col-9@lg o-col-offset-3@lg'>
-      <div className='header-container'>
+      <div className='c-maker'>
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
       </div>
