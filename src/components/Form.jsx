acb63db9f9fbb7f239125270b3cc3a09e44d4120
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 07:30:24 +0300

    rename container to div

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Form.css';
-import Container from './General/Container';
+import Div from './General/Div';
 
 const Form = () => {
   return (
-    <Container divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
+    <Div divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
       <form action='/' method='POST'>
-        <Container divs={['o-flexy@lg o-flexy--top form__bar']}>
-          <Container divs={['o-flexy__block form__block tx-center', 'o-inline']}>
-            <Container divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like </Container>
-            <Container divs={['o-inline__item u-mrg-r-1']}>
+        <Div divs={['o-flexy@lg o-flexy--top form__bar']}>
+          <Div divs={['o-flexy__block form__block tx-center', 'o-inline']}>
+            <Div divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like </Div>
+            <Div divs={['o-inline__item u-mrg-r-1']}>
               <input
                 className='c-input c-input-static--lg form-input__mini'
                 name='numberOfQuotes'
@@ -18,13 +18,13 @@ const Form = () => {
                 max='101'
                 min='1'
               />
-            </Container>
-            <Container divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Container>
-          </Container>
+            </Div>
+            <Div divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Div>
+          </Div>
           <input type='submit' value='Receive the West' className='o-flexy__item form__button js-input-bg generate-button' />
-        </Container>
+        </Div>
       </form>
-    </Container>
+    </Div>
   );
 };
 
