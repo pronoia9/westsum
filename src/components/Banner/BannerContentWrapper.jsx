fdf6d2946b82c332ba31c4abfe5d57c6ed563783
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:23:19 +0300

    use div component for nested divs

--- src/components/Banner/BannerContentWrapper.jsx
+++ src/components/Banner/BannerContentWrapper.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 import Maker from './BannerContentWrapper/Maker';
 import Header from './BannerContentWrapper/Header';
 import Image from './BannerContentWrapper/Image';
+import Div from '../General/Div';
 
-const BannerContentWrapper = () => {
+export default function BannerContentWrapper() {
   return (
-    <div className='o-container u-post-relative u-zi-10'>
-      <div className='o-row u-pos-relative'>
-        <Maker url1='' url2='https://www.seanhalpin.design/work/jeffsum/' />
-        <Header title='Westsum' subtitle='A little quote generator of Kanye West.' />
-        <Image />
-      </div>
-    </div>
+    <Div divs={['o-container u-post-relative u-zi-10', 'o-row u-pos-relative']}>
+      <Maker url1='' url2='https://www.seanhalpin.design/work/jeffsum/' />
+      <Header title='Westsum' subtitle='A little quote generator of Kanye West.' />
+      <Image />
+    </Div>
   );
-};
-
-export default BannerContentWrapper;
+}
