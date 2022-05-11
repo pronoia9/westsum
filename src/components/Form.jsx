7de1c7f5ee63594ea3fb50b0841598109fbd5159
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 06:19:14 +0300

    change nested divs into container component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Container from './Container';
 
 const Form = () => {
-  return <div className='form-container'></div>;
+  return (
+    <Container divs={['form-container', 'o-container', 'o-row', 'o-col-8@lg o-col-offset-4@lg']}>
+      <form action='/' method='POST'>
+        <Container
+          divs={['o-flexy@lg o-flexy--top form-container__bar', 'o-flexy__block form-container__block tx-center', 'o-inline']}>
+          <Container divs={['o-inline__item u-mrg-r-1', 'c-input-static c-input-static--lg']}>Gimme like</Container>
+          <Container divs={['o-inline__item u-mrg-r-1']}>
+            <input
+              class='c-input c-input-static--md form-input__mini'
+              name='numberOfQuotes'
+              value='5'
+              type='number'
+              max='101'
+              min='1'></input>
+          </Container>
+          <Container divs={['o-inline__item', 'c-input-static c-input-static--lg']}>quotes of that Kanye West.</Container>
+        </Container>
+      </form>
+    </Container>
+  );
 };
 
 export default Form;
