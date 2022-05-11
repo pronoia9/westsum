4615225258355312af0f975b3dc4a469747eb27d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 14:10:32 +0300

    fix quote

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,7 @@ import Div from './General/Div';
 export default function Content({ quotes }) {
   return (
     <Div divs={['c-content', 'o-container', 'o-row', 'o-col-10 o-col-offset-1 o-col-8@md o-col-offset-2@md', 'generated-text']}>
-      {quotes.map(q => <p>q</p>)}
+      {quotes.map(q => <p>{q}</p>)}
     </Div>
   );
 }
