17216a2dc7cdf28321b01cf1fe4a4c5984a6b323
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-11 16:18:28 +0300

    add gsap containers

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,6 +9,8 @@ const Banner = (props) => {
   return (
     <div className='banner-container banner-container--one u-pos-relative'>
       <BannerContentWrapper />
+      <div className='c-hero__kanye c-hero__kanye--two js-hero-kanye-two'></div>
+      <div className='c-hero__kanye c-hero__kanye--three js-hero-kanye-three'></div>
     </div>
   );
 };
