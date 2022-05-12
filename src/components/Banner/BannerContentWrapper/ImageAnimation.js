87ffbb6b0dd4c780d43c1bfbde4b81b76f138b3d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-12 08:51:28 +0300

    fix old/commented code

--- src/components/Banner/BannerContentWrapper/ImageAnimation.js
+++ src/components/Banner/BannerContentWrapper/ImageAnimation.js
@@ -40,27 +40,27 @@ export default function animation() {
 // (function ($) {
 //   'use strict';
 //   var tl = new TimelineLite({ onComplete: function () { this.restart(); }, });
-//   TweenLite.set('.js-jeff-img-2', { scale: 1.2, transformOrign: '100% 100%' });
-//   TweenLite.set('.js-jeff-img-3', { scale: 1.2, transformOrign: '100% 100%' });
+//   TweenLite.set('.js-kanye-img-2', { scale: 1.2, transformOrign: '100% 100%' });
+//   TweenLite.set('.js-kanye-img-3', { scale: 1.2, transformOrign: '100% 100%' });
 //   tl.delay(2);
 //   tl.add([
-//     TweenLite.to('.js-jeff-img-1', 0.5, { opacity: 0, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-1', 0.5, { opacity: 0, ease: Power0.easeNone }),
 //     TweenLite.to('.js-input-bg', 0.5, { backgroundColor: '#6C429B', ease: Power0.easeNone }),
-//     TweenLite.to('.js-hero-jeff-two', 1, { opacity: 1, ease: Power0.easeNone }),
-//     TweenLite.to('.js-jeff-img-2', 5, { scale: 1, ease: Power1.easeOut }),
+//     TweenLite.to('.js-hero-kanye-two', 1, { opacity: 1, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-2', 5, { scale: 1, ease: Power1.easeOut }),
 //   ]);
 //   tl.add([
-//     TweenLite.to('.js-jeff-img-2', 0.5, { opacity: 0, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-2', 0.5, { opacity: 0, ease: Power0.easeNone }),
 //     TweenLite.to('.js-input-bg', 0.5, { backgroundColor: '#930560', ease: Power0.easeNone }),
-//     TweenLite.to('.js-hero-jeff-three', 1, { opacity: 1, ease: Power0.easeNone }),
-//     TweenLite.to('.js-jeff-img-1', 0.5, { scale: 1.2, ease: Power0.easeNone }),
-//     TweenLite.to('.js-jeff-img-3', 5, { scale: 1, ease: Power1.easeOut }),
+//     TweenLite.to('.js-hero-kanye-three', 1, { opacity: 1, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-1', 0.5, { scale: 1.2, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-3', 5, { scale: 1, ease: Power1.easeOut }),
 //   ]);
 //   tl.add([
-//     TweenLite.to('.js-jeff-img-1', 0.5, { opacity: 1, ease: Power0.easeNone }),
-//     TweenLite.to('.js-hero-jeff-three', 1, { opacity: 0, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-1', 0.5, { opacity: 1, ease: Power0.easeNone }),
+//     TweenLite.to('.js-hero-kanye-three', 1, { opacity: 0, ease: Power0.easeNone }),
 //     TweenLite.to('.js-input-bg', 0.5, { backgroundColor: '#0551B4', ease: Power0.easeNone }),
-//     TweenLite.to('.js-hero-jeff-two', 0.5, { opacity: 0, ease: Power0.easeNone }),
-//     TweenLite.to('.js-jeff-img-1', 5, { scale: 1, ease: Power1.easeOut }),
+//     TweenLite.to('.js-hero-kanye-two', 0.5, { opacity: 0, ease: Power0.easeNone }),
+//     TweenLite.to('.js-kanye-img-1', 5, { scale: 1, ease: Power1.easeOut }),
 //   ]);
 // })(jQuery);
