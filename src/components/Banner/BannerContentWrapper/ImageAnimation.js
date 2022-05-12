e40a7bba84887d7d58057f6e70ead539ac4e224e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-12 05:40:41 +0300

    format

--- src/components/Banner/BannerContentWrapper/ImageAnimation.js
+++ src/components/Banner/BannerContentWrapper/ImageAnimation.js
@@ -8,23 +8,23 @@ export default function animation() {
   t1.delay(2);
   
   t1.add([
-    gsap.to('.js-kanye-img-1', 0.5, { opacity: 0, ease: 'power0.easeNone' }),
-    gsap.to('.js-input-bg', 0.5, { backgroundColor: '#6C429B', ease: 'power0.easeNone' }),
-    gsap.to('.js-hero-kanye-two', 1, { opacity: 1, ease: 'power0.easeNone' }),
-    gsap.to('.js-kanye-img-2', 5, { scale: 1, ease: 'power1.easeOut' }),
+    gsap.to('.js-kanye-img-1',      0.5,  { opacity: 0,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-input-bg',         0.5,  { backgroundColor: '#6C429B', ease: 'power0.easeNone' }),
+    gsap.to('.js-hero-kanye-two',   1,    { opacity: 1,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-kanye-img-2',      5,    { scale: 1,                   ease: 'power1.easeOut' }),
   ]);
   t1.add([
-    gsap.to('.js-kanye-img-2', 0.5, { opacity: 0, ease: 'power0.easeNone' }),
-    gsap.to('.js-input-bg', 0.5, { backgroundColor: '#930560', ease: 'power0.easeNone' }),
-    gsap.to('.js-hero-kanye-three', 1, { opacity: 1, ease: 'power0.easeNone' }),
-    gsap.to('.js-kanye-img-1', 0.5, { scale: 1.2, ease: 'power0.easeNone' }),
-    gsap.to('.js-kanye-img-3', 5, { scale: 1, ease: 'power1.easeOut' }),
+    gsap.to('.js-kanye-img-2',      0.5,  { opacity: 0,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-input-bg',         0.5,  { backgroundColor: '#930560', ease: 'power0.easeNone' }),
+    gsap.to('.js-hero-kanye-three', 1,    { opacity: 1,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-kanye-img-1',      0.5,  { scale: 1.2,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-kanye-img-3',      5,    { scale: 1,                   ease: 'power1.easeOut' }),
   ]);
   t1.add([
-    gsap.to('.js-kanye-img-1', 0.5, { opacity: 1, ease: 'power0.easeNone' }),
-    gsap.to('.js-hero-kanye-three', 1, { opacity: 0, ease: 'power0.easeNone' }),
-    gsap.to('.js-input-bg', 0.5, { backgroundColor: '#0551B4', ease: 'power0.easeNone' }),
-    gsap.to('.js-hero-kanye-two', 0.5, { opacity: 0, ease: 'power0.easeNone' }),
-    gsap.to('.js-kanye-img-1', 5, { scale: 1, ease: 'power1.easeOut' }),
+    gsap.to('.js-kanye-img-1',      0.5,  { opacity: 1,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-hero-kanye-three', 1,    { opacity: 0,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-input-bg',         0.5,  { backgroundColor: '#0551B4', ease: 'power0.easeNone' }),
+    gsap.to('.js-hero-kanye-two',   0.5,  { opacity: 0,                 ease: 'power0.easeNone' }),
+    gsap.to('.js-kanye-img-1',      5,    { scale: 1,                   ease: 'power1.easeOut' }),
   ]);
 }
