f901e216c19390a8832c27ca2e25923f2ce1e36f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 16:06:30 +0300

    implement typed.js

--- src/components/Banner/BannerContentWrapper/Header.jsx
+++ src/components/Banner/BannerContentWrapper/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import './Header.scss';
+import React, { useEffect } from 'react';
+import Typed from 'typed.js';
 import Div from '../../../utils/Div';
+import './Header.scss';
 
 export default function Header({ title, subtitle }) {
+  useEffect(() => {
+    const typed = new Typed('#typed-strings', {
+      strings: ['A little quote generator of Kanye West awesomeness.', subtitle],
+      typeSpeed: 40,
+      backSpeed: 50,
+    });
+    return () => typed.destroy(); // Destropying
+  }, [subtitle]);
+
   return (
     <Div divs={['o-col-10 o-col-offset-1 o-col-9@lg o-col-offset-3@lg', 'c-hero__desc']}>
       <h1>{title}</h1>
-      <h2>{subtitle}</h2>
+      <h2 id='typed-strings'>{/* eslint-disable-next-line jsx-a11y/heading-has-content */}</h2>
     </Div>
   );
 }
