633e9f2bcdec593e69d9ea6862dcce28e7a5940a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 16:09:59 +0300

    add animate class

--- src/components/Banner/BannerContentWrapper/Header.jsx
+++ src/components/Banner/BannerContentWrapper/Header.jsx
@@ -15,7 +15,7 @@ export default function Header({ title, subtitle }) {
 
   return (
     <Div divs={['o-col-10 o-col-offset-1 o-col-9@lg o-col-offset-3@lg', 'c-hero__desc']}>
-      <h1>{title}</h1>
+      <h1 className='animate__animated animate__lightSpeedInRight'>{title}</h1>
       <h2 id='typed-strings'>{/* eslint-disable-next-line jsx-a11y/heading-has-content */}</h2>
     </Div>
   );
