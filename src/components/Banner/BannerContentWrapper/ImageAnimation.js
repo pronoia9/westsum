246ef71232d496d96109e1f829fca33836a057fd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 14:40:05 +0300

    add todo comment

--- src/components/Banner/BannerContentWrapper/ImageAnimation.js
+++ src/components/Banner/BannerContentWrapper/ImageAnimation.js
@@ -1,4 +1,4 @@
-'use strict';
+// TODO: FIX GSAP ANIMATIONS
 import { gsap } from 'gsap';
 import { ExpoScaleEase, RoughEase, SlowMo } from 'gsap/EasePack';
 import { CustomEase } from 'gsap/CustomEase';
