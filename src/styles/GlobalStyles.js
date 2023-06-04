4bf2fb3b645be4d81868bfdb88262a1f60d797d4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-04 20:11:32 +0300

    add font smoothing

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -6,9 +6,13 @@ export const GlobalStyles = createGlobalStyle`
   ${resetCSS}
 
   body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     transition: background 0.2s ease-in, color 0.2s ease-in;
+
+    -moz-osx-font-smoothing: antialiased !important;
+    -webkit-font-smoothing: antialiased !important;  
   }
 `;
 
