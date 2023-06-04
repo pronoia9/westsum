d58ccbdf2887fd8972d1777353e01a836d2edf60
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-05 01:39:05 +0300

    add gsap animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { gsap } from 'gsap';
 
 import { Hero, InputBar, Quotes, ThemeButton } from './components';
 import { GlobalStyles } from './styles/GlobalStyles';
@@ -10,15 +11,56 @@ const App = () => {
   // STATE
   const [theme, setTheme] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
   // REFS
-  const imageRefs = useRef([]), backgroundRefs = useRef([]);
-  useEffect(() => { console.log(imageRefs) }, [imageRefs]);
-  useEffect(() => { console.log(backgroundRefs) }, [backgroundRefs]);
+  const imageRefs = useRef([]),
+    backgroundRefs = useRef([]),
+    buttonRef = useRef();
+
+  useLayoutEffect(() => {
+    const tl = gsap.timeline({
+      onComplete: function () {
+        this.restart();
+      },
+    });
+
+    // INIT
+    tl.set(imageRefs.current[0], { scale: 1 }, 0);
+    tl.set(imageRefs.current[1], { scale: 1.2, transformOrigin: '100% 100%' }, 0);
+    tl.set(imageRefs.current[2], { scale: 1.2, transformOrigin: '100% 100%' }, 0);
+    tl.set(backgroundRefs.current[0], { opacity: 1 }, 0);
+    tl.set(backgroundRefs.current[1], { opacity: 0 }, 0);
+    tl.set(backgroundRefs.current[2], { opacity: 0 }, 0);
+
+    tl.delay(2);
+
+    // #1 -> #2
+    tl.to(imageRefs.current[0], 2, { opacity: 0 }, 0);
+    tl.to(imageRefs.current[1], 5, { opacity: 1, scale: 1 }, 0);
+    tl.to(buttonRef.current, 0.5, { backgroundColor: '#6C429B' }, 0);
+    tl.to(backgroundRefs.current[1], 1, { opacity: 1 }, 0);
+
+    tl.set(imageRefs.current[0], { scale: 1.2, transformOrigin: '100% 100%' }, 2);
+
+    // #2 -> #3
+    tl.to(imageRefs.current[1], 2, { opacity: 0 }, 5);
+    tl.to(imageRefs.current[2], 5, { opacity: 1, scale: 1 }, 5);
+    tl.to(buttonRef.current, 0.5, { backgroundColor: '#930560' }, 5);
+    tl.to(backgroundRefs.current[2], 1, { opacity: 1 }, 5);
+    tl.set(backgroundRefs.current[1], { opacity: 0 }, 7);
+
+    // #3 -> #1
+    tl.to(imageRefs.current[2], 2, { opacity: 0 }, 10);
+    tl.to(imageRefs.current[0], 5, { opacity: 1, scale: 1 }, 10);
+    tl.to(buttonRef.current, 0.5, { backgroundColor: '#0551B4' }, 10);
+    tl.to(backgroundRefs.current[2], 1, { opacity: 0 }, 10);
+  }, []);
 
   // EVENT LISTENER FOR SYSTEM THEME CHANGE
   useEffect(() => {
     const systemThemeWatcher = window.matchMedia('(prefers-color-scheme: dark)');
     systemThemeWatcher.addEventListener('change', () => systemThemeChangeHandler(e, setTheme));
-    return () => { systemThemeWatcher.removeEventListener('change', systemThemeChangeHandler); };
+    return () => {
+      systemThemeWatcher.removeEventListener('change', systemThemeChangeHandler);
+    };
   }, []);
 
   return (
@@ -27,7 +69,7 @@ const App = () => {
         <GlobalStyles />
         <ThemeButton />
         <Hero imageRefs={imageRefs} backgroundRefs={backgroundRefs} />
-        <InputBar />
+        <InputBar buttonRef={buttonRef} />
         <Quotes />
       </>
     </ThemeProvider>
