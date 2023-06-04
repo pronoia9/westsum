e4e19f35bc4cbd6f18531bda41fa7fdc5828b73e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-04 18:28:31 +0300

    add init data

---
+++ src/utils/data.js
@@ -0,0 +1,31 @@
+import {
+  kanye1,
+  kanye2,
+  kanye3,
+  kanyemoji1,
+  kanyemoji2,
+  kanyemoji3,
+  kanyemoji4,
+  kanyemoji5,
+  kanyemoji6,
+  kanyemoji7,
+  kanyemoji8,
+  kanyemoji9,
+} from '../assets';
+
+export const portraitImgs = [
+  {
+    img: kanye1,
+    bg: '',
+  },
+  {
+    img: kanye2,
+    bg: '',
+  },
+  {
+    img: kanye3,
+    bg: '',
+  },
+];
+
+export const kanyemojis = [kanyemoji1, kanyemoji2, kanyemoji3, kanyemoji4, kanyemoji5, kanyemoji6, kanyemoji7, kanyemoji8, kanyemoji9];
