6f33e83b6162b02cf9a5c6af9941fa5f0c1376c5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-04 20:38:18 +0300

    add init hero bg

--- src/components/HeroBackground.jsx
+++ src/components/HeroBackground.jsx
@@ -1,3 +1,37 @@
+import styled from 'styled-components';
+
 export default function HeroBackground() {
-  return <div>HeroBackground</div>;
+  return (
+    <>
+      <Background className={`one`} />
+    </>
+  );
 }
+
+const Background = styled.div`
+  pointer-events: none;
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+
+  &.one {
+    background-image: -webkit-linear-gradient(152deg, #61bfd9 16%, #0551b4 67%);
+    background-image: -o-linear-gradient(152deg, #61bfd9 16%, #0551b4 67%);
+    background-image: linear-gradient(242deg, #61bfd9 16%, #0551b4 67%);
+  }
+
+  &.two {
+    background-image: -webkit-linear-gradient(152deg, #15af88 16%, #6c429a 67%);
+    background-image: -o-linear-gradient(152deg, #15af88 16%, #6c429a 67%);
+    background-image: linear-gradient(242deg, #15af88 16%, #6c429a 67%);
+  }
+
+  &.three {
+    background-image: -webkit-linear-gradient(152deg, #eea031 18%, #930560 67%);
+    background-image: -o-linear-gradient(152deg, #eea031 18%, #930560 67%);
+    background-image: linear-gradient(242deg, #eea031 18%, #930560 67%);
+  }
+`;
