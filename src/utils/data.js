475dded9474727b57330b5816bf7dd630e2c8722
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-04 20:32:53 +0300

    change hero image data

--- src/utils/data.js
+++ src/utils/data.js
@@ -13,19 +13,6 @@ import {
   kanyemoji9,
 } from '../assets';
 
-export const portraitImgs = [
-  {
-    img: kanye1,
-    bg: '',
-  },
-  {
-    img: kanye2,
-    bg: '',
-  },
-  {
-    img: kanye3,
-    bg: '',
-  },
-];
+export const heroImages = [kanye1, kanye2, kanye3];
 
 export const kanyemojis = [kanyemoji1, kanyemoji2, kanyemoji3, kanyemoji4, kanyemoji5, kanyemoji6, kanyemoji7, kanyemoji8, kanyemoji9];
