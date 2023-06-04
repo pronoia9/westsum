6b3fcb903155813db94ec20b64d94532cfaf2f92
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-04 18:46:29 +0300

    init themebutton component

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,10 @@
+export default function ThemeButton() {
+  return (
+    <>
+      {/* <button onClick={toggleTheme}>
+          {isDarkTheme ? (<span aria-label='Light mode' role='img'>🌞</span>) : (<span aria-label='Dark mode' role='img'>🌜</span>
+          )}
+        </button> */}
+    </>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Hero from './Hero';
 import InputBar from './InputBar';
 import Portrait from './Portrait';
 import Quotes from './Quotes';
+import ThemeButton from './ThemeButton';
 
-export { Hero, InputBar, Portrait, Quotes };
+export { Hero, InputBar, Portrait, Quotes, ThemeButton };
