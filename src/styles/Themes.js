cf456263776fbfcd6e71ed3e63c15c16a0121f15
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-05 17:23:15 +0300

    add og values

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -1,15 +1,23 @@
 // https://colorhunt.co/palette/27374d526d829db2bfdde6ed
+const original = {
+  darkest: '#27374D',
+  dark: '#526D82',
+  light: '#DDE6ED',
+  lightest: '#9DB2BF',
+};
 
 export const lightTheme = {
-  background: '#DDE6ED',
-  background2: '#9DB2BF',
-  font: '#27374D',
-  font2: '#526D82',
+  background: original.light,
+  background2: original.lightest,
+  font: original.darkest,
+  font2: original.dark,
+  inputBarBg: original.light,
 };
 
 export const darkTheme = {
-  background: '#27374D',
-  background2: '#526D82',
-  font: '#DDE6ED',
-  font2: '#9DB2BF',
+  background: original.darkest,
+  background2: original.dark,
+  font: original.light,
+  font2: original.lightest,
+  inputBarBg: original.dark,
 };
