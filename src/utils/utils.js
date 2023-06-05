81cc3d2265a65dfa5307bb733d7a6fb385d3beca
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-05 17:11:29 +0300

    fix theme func props

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 export const isDarkTheme = (theme) => theme === 'dark';
 
-export const toggleTheme = (setTheme) => setTheme(isDarkTheme() ? 'light' : 'dark');
+export const toggleTheme = (theme, setTheme) => setTheme(isDarkTheme(theme) ? 'light' : 'dark');
 
 export const systemThemeChangeHandler = (e, setTheme) => { setTheme(e.matches ? 'dark' : 'light'); };
