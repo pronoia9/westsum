35226c819e4892661311dfe85e28ed64fb3c15c8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-05 17:14:36 +0300

    format

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,12 +37,6 @@ const OContainer = styled.div`
   max-width: 1140px;
   padding-left: 15px;
   padding-right: 15px;
-
-  /* &.fluid { max-width: 100%; }
-  &.responsive { max-width: 100%; }
-  @media (min-width: 544px) { &.responsive {  max-width: 576px; } }
-  @media (min-width: 768px) { &.responsive { max-width: 720px; } } 
-  @media (min-width: 992px) { &.responsive { max-width: 940px; } } */
 `;
 
 const ORow = styled.div`
